c0500516e7e82f3fa78fed572fe0f1cfdf97ef65
Adam Luong <adam.luong84@gmail.com>
2013-04-03 17:23:08 +0700

    original version
    new added

---
+++ AlexTran/DEV10102/CardTransactionSearchServiceCommand.cs
@@ -0,0 +1,95 @@
+﻿using Common.Contracts.Prepaid.Records;
+using Common.Contracts.Prepaid.Requests;
+using Common.Contracts.Prepaid.Responses;
+using Payjr.Core.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payjr.Core.ServiceCommands.Prepaid
+{
+    public class CardTransactionSearchServiceCommand : ProviderServiceCommandBase<RetrieveTransactionRequest, RetrieveTransactionResponse>
+    {
+        private string _cardIdentifier;
+        private DateTime _endDate;
+        private bool _filteredForUserViewing;
+        private bool _includeReplacementCards;
+        public int _numberPerPage;
+        public int _pageNumber;
+        private DateTime _startDate;
+         
+        public CardTransactionSearchServiceCommand(IProviderFactory providers) : base(providers) { }
+
+        protected override bool OnExecute(RetrieveTransactionResponse response)
+        {
+            // cai dat code, co the xay ra loi, exception o day
+            //response.CardTransactions.Add
+            //(
+            //    new CardTransactionRecord
+            //    {
+            //        AccountIdentifier = new CreditCardIdentifier(cardAcc.AccountID).ToString(),
+            //        CardNumberLastFour = cardAcc.AccountNumber.Substring(cardAcc.AccountNumber.Length - 4),
+            //        CardType = cardAcc.Type,
+            //        ExpirationMonth = cardAcc.ExpirationDate.ToString("MM"),
+            //        ExpirationYear = cardAcc.ExpirationDate.ToString("yyyy"),
+            //        User = new UserDetailRecord(),
+            //        UserIdentifier = new UserIdentifier(cardAcc.UserID.Value).Identifier
+            //    }
+            //);
+            return true;
+        }
+
+        //public string ActingUserIdentifier { get; set; }
+        //public decimal Amount { get; set; }
+        //public DateTime Date { get; set; }
+        //public string Description { get; set; }
+        //public string LongTransactionTypeDescription { get; set; }
+        //public string MerchantCategoryGroup { get; set; }
+        //public decimal RunningBalance { get; set; }
+        //public string ShortTransactionTypeDescription { get; set; }
+        //public string TransactionNumber { get; set; }
+
+        protected override void Validate(RetrieveTransactionRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request", "request must be set");
+            }
+            if (string.IsNullOrWhiteSpace(request.CardIdentifier))
+            {
+                throw new ArgumentException("CardIdentifier must be set", "request.CardIdentifier");
+            }
+            if (request.Configuration == null)
+            {
+                 throw new ArgumentException("request.Configuration must be set", "request");
+            }
+            if (!request.Configuration.ApplicationKey.HasValue)
+            {
+                throw new ArgumentException("request.Configuration.ApplicationKey must be set", "request");
+            }                      
+            if ( request.NumberPerPage<=0)
+            {
+                throw new ArgumentException("request.NumberPerPage must be set", "request");
+            }
+            if ( request.PageNumber <= 0)
+            {
+                throw new ArgumentException("request.PageNumber must must be set", "request");
+            }
+            int _result = DateTime.Compare(request.StartDate, request.EndDate);
+            if (_result > 0)
+            {
+                throw new ArgumentException("request.StartDate must earlier than is request.EndDate", "request");
+            }
+          
+            _cardIdentifier = request.CardIdentifier;
+            _endDate = request.EndDate;
+            _startDate = request.StartDate;
+            _filteredForUserViewing = request.FilteredForUserViewing;
+            _includeReplacementCards = request.IncludeReplacementCards;
+            _numberPerPage = request.NumberPerPage;
+            _pageNumber = request.PageNumber;
+        }
+    }
+}
