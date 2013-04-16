d2a4e4a7e36ba2369e6b4e2507cb51f6234c744c
Lucas Tran <tranhanvien@gmail.com>
2013-04-17 00:40:03 +0700

    review servicecommand and unitest
    review lan 2 cho task SendToFulfillment

--- LucasTran/DEV10101/SendToFulfillmentServiceCommand.cs
+++ LucasTran/DEV10101/SendToFulfillmentServiceCommand.cs
@@ -2,142 +2,78 @@
 using Common.Contracts.ProductFulfillment.Records;
 using Common.Contracts.ProductFulfillment.Requests;
 using Common.Contracts.ProductFulfillment.Responses;
+using Payjr.Core.FinancialAccounts;
+using Payjr.Core.Identifiers;
 using Payjr.Core.Providers;
+using Payjr.Core.Users;
+using SERVICES = Payjr.Core.Services.UserService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Business.Validation;
+using Payjr.Types;
+using Payjr.Core.Services;
 
 
 namespace Payjr.Core.ServiceCommands.ProductFulfillment
 {
     public class SendToFulfillmentServiceCommand : ProviderServiceCommandBase<SendToFulfillmentRequest,SendToFulfillmentResponse>
     {
-        SendToFulfillmentRecord fulfillmentRecords = new SendToFulfillmentRecord();
+        List<SendToFulfillmentRecord> _fulfillmentRecords = new List<SendToFulfillmentRecord>();
 
         public SendToFulfillmentServiceCommand(IProviderFactory providers) : base(providers) { }
 
         protected override bool OnExecute(SendToFulfillmentResponse response)
         {
-			return true;// nho Adam review
+            //Payjr.Entity.EntityClasses.UserEntity
+            Teen teen;
+            Guid user;
+            PrepaidCardAccount prepaidAccount;
+            foreach (SendToFulfillmentRecord record in _fulfillmentRecords)
+            {
+                user = new UserIdentifier(record.UserIdentifier).ID;
+                teen = User.RetrieveUser(user) as Teen;
+                //ServiceFactory.UserConfiguration.AssignProductToUser(Product.PPaid_IC, teen);
+                prepaidAccount = teen.NewPrepaidCardAccount();
+                //prepaidAccount.IsActive = true;
+                teen.Save(null);
+                response.ResponseRecords.Add(record);
+            }
+            
+            return true;
         }
 
         protected override void Validate(SendToFulfillmentRequest request)
         {
-			if (request == null)
+            if (request == null)
             {
-                throw new ArgumentNullException("request", "request must be set");
+                throw new ValidationException("request must be set");
             }
-            if (request.Configuration == null)
+            
+            var sendToFulfillmentRecords = request.RequestRecords;
+            if (sendToFulfillmentRecords.Count == 0)
             {
-                throw new ArgumentException("request.Configuration must be set", "request");
+                throw new ValidationException("request.RequestRecords must not be null or empty");
             }
-            if (request.Configuration.ApplicationKey == null)
-            {
-                throw new ArgumentException("request.Configuration.ApplicationKey must be set", "request");
-            }
-            if (request.Header == null)
-            {
-                throw new ArgumentException("request.Header must be set", "request");
-            }
-            if (request.Header.CallerName == null)
-            {
-                throw new ArgumentException("request.Header.CallerName must be set", "request");
-            }
+            SendToFulfillmentRecord r = new SendToFulfillmentRecord();
 
-            if (request.RequestRecords == null)
+            int i = 0;
+            foreach (SendToFulfillmentRecord record in sendToFulfillmentRecords)
             {
-                throw new ArgumentException("request.RequestRecords must be set", "request");
-            }
-            if (request.RequestRecords.Count <= 0)
-            {
-                throw new ArgumentException("request.RequestRecords must have one record", "request");
-            }
-            SendToFulfillmentRecord fulfillmentRecord = request.RequestRecords[0];
-            if (String.IsNullOrWhiteSpace(fulfillmentRecord.CustomerType) == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].CustomerType must be set", "request");
-            }
-            if (String.IsNullOrWhiteSpace(fulfillmentRecord.Ref1) == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].Ref1 must be set", "request");
-            } 
-            if (String.IsNullOrWhiteSpace(fulfillmentRecord.UserIdentifier) == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].UserIdentifier must be set", "request");
-            }
-
-            if (fulfillmentRecord.ProductLineItems == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].ProductLineItems must be set", "request");
-            }
-            if(fulfillmentRecord.ProductLineItems.Count <= 0)
-            {
-                throw new ArgumentException("request.RequestRecords[0].ProductLineItems must have record", "request");
-            }
-            foreach (ProductFulfillmentLineItem item in fulfillmentRecord.ProductLineItems)
-            {
-                if (item.Configuration == null)
+                var productFulfillmentLineItems = record.ProductLineItems;
+                if (productFulfillmentLineItems.Count == 0)
                 {
-                    throw new ArgumentException("request.RequestRecords[0].ProductLineItems.Configuration must be set", "request");
+                    throw new ValidationException(string.Format("request.RequestRecords[{0}].ProductLineItems must not be null or empty", i));
                 }
-                if (String.IsNullOrWhiteSpace(item.LineItemIdentifier))
+                if (string.IsNullOrWhiteSpace(record.UserIdentifier))
                 {
-                    throw new ArgumentException("request.RequestRecords[0].ProductLineItems.LineItemIdentifier must be set", "request");
+                    throw new ArgumentException(string.Format("request.RequestRecords[{0}].UserIdentifier must be set",i));
                 }
-                if (item.ProductRecords == null)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].ProductLineItems.ProductRecords must be set", "request");
-                }
+                i++;
+                _fulfillmentRecords.Add(record);
             }
-
-            if (fulfillmentRecord.ShipmentPackaging == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].ShipmentPackaging must be set", "request");
-            }
-            if(fulfillmentRecord.ShipmentPackaging.Count <= 0)
-            {
-                throw new ArgumentException("request.RequestRecords[0].ShipmentPackaging must have record", "request");
-            }
-
-            foreach (ShipmentPackaging item in fulfillmentRecord.ShipmentPackaging)
-            {
-                if (item.ShippingMethod == null)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].ShipmentPackaging.ShippingMethod must be set", "request");
-                }
-                if (item.LineItemBundles == null)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].ShipmentPackaging.LineItemBundles must be set", "request");
-                }
-            }
-
-            if (fulfillmentRecord.TransactionRecords == null)
-            {
-                throw new ArgumentException("request.RequestRecords[0].TransactionRecords must be set", "request");
-            }
-            if(fulfillmentRecord.TransactionRecords.Count <= 0)
-            {
-                throw new ArgumentException("request.RequestRecords[0].TransactionRecords must have record", "request");
-            }
-            foreach (Common.Contracts.ProductFulfillment.Records.TransactionRecord item in fulfillmentRecord.TransactionRecords)
-            {
-                if (item.Amount < 0)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].TransactionRecords.Amount must be positive or zero ", "request");
-                }
-                if(item.PaymentType == null)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].TransactionRecords.PaymentType must be set", "request");
-                }
-                if(item.TransactionIdentifier == null)
-                {
-                    throw new ArgumentException("request.RequestRecords[0].TransactionRecords.TransactionIdentifier must be set", "request");
-                }
-            }
-
-            fulfillmentRecords = request.RequestRecords[0];
         }
     }
 }
---
+++ LucasTran/DEV10101/SendToFulfillmentServiceCommandTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common.Contracts.ProductFulfillment.Requests;
+using Payjr.Core.ServiceCommands.ProductFulfillment;
+using Common.Contracts.ProductFulfillment.Records;
+using Common.Contracts.Shared.Records;
+using System.Collections.Generic;
+using Common.Contracts.OrderProcessing.Records;
+
+namespace Payjr.Core.Test.ServiceCommands.ProductFulfillment
+{
+    [TestClass]
+    public class SendToFulfillmentServiceCommandTest : TestBase2
+    {
+        
+        [TestMethod]
+        public void Execute_Failure_RequestIsNull()
+        {
+            SendToFulfillmentRequest request = null;
+            var target = new SendToFulfillmentServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.IsNotNull(result.Status);
+            Assert.IsFalse(result.Status.IsSuccessful);
+            Assert.AreEqual("request must be set", result.Status.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Execute_Successful()
+        {
+            SendToFulfillmentRequest request = CreateSendToFulfillmentRequest();
+            var target = new SendToFulfillmentServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.IsNotNull(result.Status);
+            Assert.IsTrue(result.Status.IsSuccessful);
+            Assert.AreEqual(request.RequestRecords.Count, result.ResponseRecords.Count);
+        }
+
+        [TestMethod]
+        public void Execute_Failure_RequestRecordsIsEmpty()
+        {
+            SendToFulfillmentRequest request = new SendToFulfillmentRequest();
+
+            var target = new SendToFulfillmentServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+
+            Assert.AreEqual(0, result.ResponseRecords.Count);
+            Assert.IsNotNull(result.Status);
+            Assert.IsFalse(result.Status.IsSuccessful);
+            Assert.AreEqual("request.RequestRecords must not be null or empty",result.Status.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Execute_Failure_ProductLineItemsIsEmpty()
+        {
+            SendToFulfillmentRequest request = CreateSendToFulfillmentRequest_ProductLineItemsIsEmpty();
+                   
+            var target = new SendToFulfillmentServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+
+            Assert.AreNotEqual(0, request.RequestRecords.Count);
+
+            int i = 0;
+            foreach (SendToFulfillmentRecord record in request.RequestRecords)
+            {
+                Assert.AreEqual(0, record.ProductLineItems.Count);
+                Assert.IsNotNull(result.Status);
+                Assert.IsFalse(result.Status.IsSuccessful);
+                Assert.AreEqual(string.Format("request.RequestRecords[{0}].ProductLineItems must not be null or empty",i),result.Status.ErrorMessage);
+                i++;
+            }
+        }
+        [TestMethod]
+        public void Execute_Failure_UserIdentifierIsNullOrWhiteSpace()
+        {
+            SendToFulfillmentRequest request = CreateSendToFulfillmentRequest_UserIdentifierIsNullOrWhiteSpace();
+
+            var target = new SendToFulfillmentServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+
+            Assert.AreNotEqual(0, request.RequestRecords.Count);
+
+            int i = 0;
+            foreach (SendToFulfillmentRecord record in request.RequestRecords)
+            {
+                Assert.AreNotEqual(0, record.ProductLineItems.Count);
+                Assert.IsNotNull(result.Status);
+                Assert.IsFalse(result.Status.IsSuccessful);
+                Assert.AreEqual(string.Format("request.RequestRecords[{0}].UserIdentifier must be set", i), result.Status.ErrorMessage);
+                i++; 
+            }
+        }
+
+        #region helper methods
+
+        private SendToFulfillmentRequest CreateSendToFulfillmentRequest()
+        {
+            var result = new SendToFulfillmentRequest
+            {
+                Header = new RequestHeaderRecord
+                {
+                    CallerName = "SendToFulfillmentServiceCommandTest"
+                }
+            };
+
+            SendToFulfillmentRecord record = new SendToFulfillmentRecord();
+            record.CustomerType = "";
+            record.ProductLineItems.Add(new ProductFulfillmentLineItem());
+            record.Ref1 = "";
+            record.ShipmentPackaging = new List<ShipmentPackaging>();
+            record.TransactionRecords = new List<Common.Contracts.ProductFulfillment.Records.TransactionRecord>();
+            record.UserIdentifier = "PAYjrUser" + _teen.UserID.ToString();
+            result.RequestRecords.Add(record);
+           
+            return result;
+        }
+
+        private SendToFulfillmentRequest CreateSendToFulfillmentRequest_ProductLineItemsIsEmpty()
+        {
+            var result = new SendToFulfillmentRequest
+            {
+                Header = new RequestHeaderRecord
+                {
+                    CallerName = "SendToFulfillmentServiceCommandTest"
+                }
+            };
+            
+            SendToFulfillmentRecord record = new SendToFulfillmentRecord();
+            record.CustomerType = "";
+            //record.ProductLineItems // IsEmpty
+            record.Ref1 = "";
+            record.ShipmentPackaging = new List<ShipmentPackaging>();
+            record.TransactionRecords = new List<Common.Contracts.ProductFulfillment.Records.TransactionRecord>();
+            record.UserIdentifier = "PAYjrUserCD8E5067-8ABB-43FB-9D66-40F961B79F6E";
+            result.RequestRecords.Add(record);
+            
+            return result;
+        }
+
+        private SendToFulfillmentRequest CreateSendToFulfillmentRequest_UserIdentifierIsNullOrWhiteSpace()
+        {
+            var result = new SendToFulfillmentRequest
+            {
+                Header = new RequestHeaderRecord
+                {
+                    CallerName = "SendToFulfillmentServiceCommandTest"
+                }
+            };
+
+            SendToFulfillmentRecord record = new SendToFulfillmentRecord();
+            record.CustomerType = "";
+            record.ProductLineItems.Add(new ProductFulfillmentLineItem());
+            record.Ref1 = "";
+            record.ShipmentPackaging = new List<ShipmentPackaging>();
+            record.TransactionRecords = new List<Common.Contracts.ProductFulfillment.Records.TransactionRecord>();
+            record.UserIdentifier = "";
+            result.RequestRecords.Add(record);
+           
+            return result;
+        }
+        #endregion
+    }
+}
