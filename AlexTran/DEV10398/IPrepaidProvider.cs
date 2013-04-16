ffee367df92ddcf667f3c10626b44e0b15a4fea8
Tran Dinh Duy <trana@cardlabcorp.com>
2013-04-16 17:49:28 +0700

    Goc

---
+++ AlexTran/DEV10398/IPrepaidProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.Contracts.Prepaid.Records;
+
+
+namespace CardLab.CMS.Providers
+{
+    public interface IPrepaidProvider : Common.Service.Providers.IServiceProvider
+    {
+        List<PrepaidCardDetailRecord> RetrieveCardDetaislByUserIdentifier(Guid applicationKey, string userIdentifier);
+        
+        bool CardActivation(Guid applicationKey, string cardIdentifier, string actingUserIdentifier, string ipAddress, string activeData);
+
+        List<CardTransactionRecord> RetrieveCardTransactions(Guid applicationKey, string cardIdentifier, DateTime startDate, DateTime endDate, int pageNumber, int numberPerPage,out int totalRecord );
+    }
+}
