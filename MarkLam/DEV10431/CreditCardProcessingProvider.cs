fd0b2cc09940e2e96e19bdd0c3b84fd4b4e05cd9
Mark Lam <lamma@carlabcorp.com>
2013-04-24 03:14:27 +0700

    DEV 10431- Original Files

---
+++ MarkLam/DEV10431/CreditCardProcessingProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CardLab.CMS.Metrics;
+using Common.Contracts.CreditCard;
+using Common.Contracts.CreditCard.Records;
+using Common.Contracts.CreditCard.Requests;
+using Common.Contracts.Shared.Records;
+using Common.Service.Providers;
+using Common.Types;
+
+namespace CardLab.CMS.Providers
+{
+    public class CreditCardProcessingProvider : ServiceProviderBase<ICreditCardProcessing, IProviderFactory, MetricRecorder>, ICreditCardProcessingProvider
+    {
+        public CreditCardProcessingProvider(IProviderFactory providerFactory, string endpointConfigurationName) : base(providerFactory, endpointConfigurationName)
+        {
+        }
+
+        public CreditCardProcessingProvider(IProviderFactory providerFactory, ICreditCardProcessing serviceOverride) : base(providerFactory, serviceOverride)
+        {
+        }
+
+        public CreditCardProcessingProvider(IProviderFactory providerFactory) : base(providerFactory)
+        {
+        }
+
+        public CreditCardDetailedRecord CreateCreditCard(Guid appicationKey, string userIdentifier, UserDetailRecord userInfo, string cardNumber, string cvv2, int expirationMonth, int expirationYear, CreditCardType cardType)
+        {
+            var addCardRecord = new AddCardRecord()
+            {
+                UserIdentifier = userIdentifier,
+                User = userInfo,
+                CardNumber = cardNumber,
+                CVV2 = cvv2,
+                ExpirationMonth = expirationMonth.ToString("G"),
+                ExpirationYear = expirationYear.ToString("G"),
+                CardType = cardType
+            };
+
+            var configurationRecord = new ConfigurationRecord()
+                                          {
+                                              ApplicationKey = appicationKey,
+                                              BrandingKey = appicationKey,
+                                              SystemConfiguration = Guid.Empty,
+                                              TenantKey = Guid.Empty
+                                          };
+
+            var request = new AddCardRequest();
+            request.AddCardsRecords.Add(addCardRecord);
+            request.Configuration = configurationRecord;
+
+            try
+            {
+                var response = CallService(() => CreateInstance().AddCard(appicationKey, request));
+
+                if (response.CreditCards.Count > 0)
+                {
+                    return response.CreditCards[0];
+                }
+            }
+            catch (Exception e)
+            {
+                Log.ErrorException("Error creation credit card", e);
+            }
+            return null;
+        }
+    }
+}
