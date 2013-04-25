39c80bda3bf521e6922229acf02f2d81f81f4736
DungMai <dungmai145@gmail.com>
2013-04-26 03:25:46 +0700

    DEV10403 review 1

---
+++ CarrickMai/DEV10403/TeenTransactionsWebPart.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CMSApp.CMSWebParts.CardLab.Buxx.Account
+{
+    public partial class TeenTransactionsWebPart : PayjrBaseWebPart
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //As a sample, need change input value
+            _listRecentTransactions.BindControl("cardIdentifier", DateTime.Now, DateTime.Now);
+        }
+    }
+}
