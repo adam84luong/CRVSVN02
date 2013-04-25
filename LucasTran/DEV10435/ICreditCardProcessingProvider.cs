3bda13ab315555f0c5fa3a6501fb21b7ff2c6890
Lucas Tran <tranhanvien@gmail.com>
2013-04-25 11:08:39 +0700

    - up 3 file goc cho taskDEV10435

---
+++ LucasTran/DEV10435/ICreditCardProcessingProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Common.Contracts.CreditCard.Records;
+using Common.Types;
+
+namespace CardLab.CMS.Providers
+{
+    public interface ICreditCardProcessingProvider : Common.Service.Providers.IServiceProvider
+    {
+        CreditCardDetailedRecord CreateCreditCard(Guid appicationKey, string userIdentifier, UserDetailRecord userInfo,
+                                                  string cardNumber, string cvv2, int expirationMonth,
+                                                  int expirationYear, CreditCardType cardType);
+    }
+}
