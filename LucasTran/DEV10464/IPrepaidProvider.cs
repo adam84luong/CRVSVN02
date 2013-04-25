d97cf7a4746cab56f390dbe0a389c6ab3aad0c93
Lucas Tran <tranhanvien@gmail.com>
2013-04-25 11:08:11 +0700

    - up file goc task DEV10646

---
+++ LucasTran/DEV10464/IPrepaidProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.Contracts.Prepaid.Records;
+
+
+namespace CardLab.CMS.Providers
+{
+    public interface IPrepaidProvider : Common.Service.Providers.IServiceProvider
+    {
+        List<PrepaidCardDetailRecord> RetrieveCardDetaislByUserIdentifierCardNumber(Guid applicationKey, string userIdentifier = null, string cardnumber = null);
+        
+        bool CardActivation(Guid applicationKey, string cardIdentifier, string actingUserIdentifier, string ipAddress, string activeData);
+
+        List<CardTransactionRecord> RetrieveCardTransactions(Guid applicationKey, string cardIdentifier, DateTime startDate, DateTime endDate, int pageNumber, int numberPerPage, out int totalRecord);
+    }
+}
