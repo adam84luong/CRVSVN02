d242aeb439271684173d0492d9c7a93c647f1d47
Tran Dinh Duy <trana@cardlabcorp.com>
2013-04-25 12:43:28 +0700

    DEV 10430 Review 2

--- AlexTran/DEV10430/CreditCardBussiness.cs
+++ AlexTran/DEV10430/CreditCardBussiness.cs
@@ -4,13 +4,15 @@ using CardLab.CMS.SiteSystem;
 using Common.Contracts.CreditCard.Records;
 using Common.Contracts.Shared.Records;
 using Common.Types;
+using CardLab.CMS.PayjrSites.DTO;
+using System.Collections.Generic;
 
 namespace CardLab.CMS.PayjrSites.Bussiness
 {
     public class CreditCardBussiness
     {
         protected IPayjrSystemInfoProvider PayjrSystem;
-        protected IProviderFactory Provider;
+        protected static IProviderFactory Provider;
 
         #region Constructor
 
@@ -52,6 +54,55 @@ namespace CardLab.CMS.PayjrSites.Bussiness
             return cc;
         }
 
+        public static List<FundingCreditCard> GetFundingCreditCard(string UserIdentifier)
+        {
+            List<CreditCardDetailedRecord> creditCardAccounts= Provider.CreditCardProcessing.RetrieveAccounts(UserIdentifier);
+            List<FundingCreditCard> fundingSourcesItems = new List<FundingCreditCard>();
+            foreach (CreditCardDetailedRecord creditCardAccount in creditCardAccounts)
+            {
+                fundingSourcesItems.Add(ConvertCreditCardtoFundingCreditCard(creditCardAccount));
+            }
+            return fundingSourcesItems;
+        }
+        public static FundingCreditCard ConvertCreditCardtoFundingCreditCard(CreditCardDetailedRecord creditCardAccount)
+        {
+            FundingCreditCard fundingSourcesItem = new FundingCreditCard();
+            fundingSourcesItem.AccountIdentifier = creditCardAccount.AccountIdentifier;
+            fundingSourcesItem.CardType = creditCardAccount.CardType.ToString();
+            fundingSourcesItem.CardNumber = "XXXX-XXXX-XXXX-" + creditCardAccount.CardNumberLastFour;
+            string status;
+            fundingSourcesItem.Expiration = GetExpiration(creditCardAccount.ExpirationMonth, creditCardAccount.ExpirationYear, out status);
+            fundingSourcesItem.Status = status;
+            return fundingSourcesItem;
+        }
+
+        public static string GetExpiration(string month, string year, out string status)
+        {
+            string expireDay = month + "/" + year.Substring(2, 2);
+            string expirestatus = "";
+            if (DateTime.Now.Year < Int32.Parse(year))
+            {
+                status = "Good";
+            }
+            else if (DateTime.Now.Year == Int32.Parse(year))
+            {
+                if (DateTime.Now.Month < Int32.Parse(month))
+                {
+                    status = "Good";
+                }
+                else
+                {
+                    status = "Bad";
+                    expirestatus = " (expired)";
+                }
+            }
+            else
+            {
+                status = "Bad";
+                expirestatus = " (expired)";
+            }
+            return expireDay + expirestatus;
+        }
         #endregion
     }
 }
--- AlexTran/DEV10430/FundingSourcesWebPart.ascx.cs
+++ AlexTran/DEV10430/FundingSourcesWebPart.ascx.cs
@@ -1,4 +1,5 @@
-﻿using CardLab.CMS.PayjrSites.User;
+﻿using CardLab.CMS.PayjrSites.Bussiness;
+using CardLab.CMS.PayjrSites.User;
 using Common.Contracts.CreditCard.Records;
 using System;
 using System.Collections.Generic;
@@ -20,70 +21,18 @@ namespace CMSApp.CMSWebParts.CardLab.Buxx.Account
                 BindGrid(parent, true);
             }
         }
-        #region Class Item FundingSources
-
-        protected class FundingSourcesItem
-        {
-            public string AccountIdentifier { get; set; }
-            public string CardType { get; set; }
-            public string CardNumber { get; set; }
-            public string Expiration { get; set; }
-            public string Status { get; set; }
-        }
-
-        #endregion
-        
+      
         #region Helper Method
               
         public void BindGrid(ParentUser parent, bool isDataBind = true)
-        {
-            List<CreditCardDetailedRecord> creditCardAccounts = parent.ProviderFactory.CreditCardProcessing.RetrieveAccounts(parent.UserIdentifier);
-            List<FundingSourcesItem> fundingSourcesItems = new List<FundingSourcesItem>();
-            foreach (CreditCardDetailedRecord creditCardAccount in creditCardAccounts)
-            {
-                FundingSourcesItem fundingSourcesItem = new FundingSourcesItem();
-                fundingSourcesItem.AccountIdentifier = creditCardAccount.AccountIdentifier;
-                fundingSourcesItem.CardType = creditCardAccount.CardType.ToString();
-                fundingSourcesItem.CardNumber = "XXXX-XXXX-XXXX-" + creditCardAccount.CardNumberLastFour;
-                string status;
-                fundingSourcesItem.Expiration = GetExpiration(creditCardAccount.ExpirationMonth, creditCardAccount.ExpirationYear, out status);
-                fundingSourcesItem.Status = status;
-                fundingSourcesItems.Add(fundingSourcesItem);
-            }
-            _fundingSourcesGrid.DataSource = fundingSourcesItems;
+        {  
+           _fundingSourcesGrid.DataSource =CreditCardBussiness.GetFundingCreditCard(parent.UserIdentifier);
             if (isDataBind)
             {
                 _fundingSourcesGrid.DataBind();
             }
         }
-        public string GetExpiration(string month, string year, out string status)
-        {
-            string expireDay = month + "/" + year.Substring(2, 2);
-            string expirestatus = "";
-            if (DateTime.Now.Year < Int32.Parse(year))
-            {
-                status = "Good";
-            }
-            else if (DateTime.Now.Year == Int32.Parse(year))
-            {
-                if (DateTime.Now.Month < Int32.Parse(month))
-                {
-                    status = "Good";
-                }
-                else
-                {
-                    status = "Bad";
-                    expirestatus = " (expired)";
-                }
-            }
-            else
-            {
-                status = "Bad";
-                expirestatus = " (expired)";
-            }
-            return expireDay + expirestatus;
-        }
-
+       
         #endregion
     }
 
