5740d2ad1346f4e7fe172c09eae6f111680eca63
Mark Lam <lamma@carlabcorp.com>
2013-04-27 14:52:19 +0700

    DEV 10502 - DeleteUserServiceCommand -Nho Adam review giup

---
+++ MarkLam/DEV10502/DeleteUserServiceCommand.cs
@@ -0,0 +1,95 @@
+﻿using Authentication.Contracts.Authentication;
+using Common.Exceptions;
+using Common.Service.Commands;
+using Payjr.Core.Metrics;
+using Payjr.Core.Providers;
+using Payjr.Core.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payjr.Core.ServiceCommands.Authentication
+{
+    public class DeleteUserServiceCommand : ProviderServiceCommand<IProviderFactory, MetricRecorder, LocalRequest, AuthServiceResponse>
+    {
+        private string _userName;
+        private bool _isdeleteAllRelatedData;
+        private User _user;
+
+        public DeleteUserServiceCommand(IProviderFactory providerFactory)
+            : base(providerFactory)
+        {
+        }
+
+        protected override bool OnExecute(AuthServiceResponse response)
+        {
+            response.Result = Result.Error;
+            try
+            {
+                if (_user.RoleType == Entity.RoleType.RegisteredTeen)
+                {
+                    Teen teen = _user as Teen;
+                    if (teen != null)
+                    {
+                        Parent parent = teen.Parent;
+                        if (parent != null)
+                        {
+                            teen.CancelService(parent.UserID);
+                            response.Result = Result.Success;
+                            response.Data = _userName;
+                        }
+                    }
+                }
+                else if (_user.RoleType == Entity.RoleType.Parent)
+                {
+                    Family family = new Family(_user);
+                    family.Delete();
+                    response.Result = Result.Success;
+                    response.Data = _userName;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Debug("Occur error while processing delete user:" + ex.Message);
+                throw new CardLabException (ex.Message);
+            }
+        }
+
+        protected override void Validate(LocalRequest request)
+        {
+            if (request == null)
+            {
+                Log.Error("Delete User can not process with request is null");
+                throw new ArgumentNullException("request");
+            }
+            if (request.Configuration == null)
+            {
+                Log.Error("Delete User can not process with request.Configuration is null");
+                throw new ArgumentException("request.Configuration must be set", "request");
+            }
+            if (!request.Configuration.ApplicationKey.HasValue)
+            {
+                Log.Error("Delete User can not process with request.Configuration.ApplicationKey has not value");
+                throw new ArgumentException("request.Configuration.ApplicationKey must be set", "request");
+            }
+            _userName = request.UserName;
+            if (string.IsNullOrWhiteSpace(_userName))
+            {
+                Log.Error("Delete User can not process with request.UserName has not value");
+                throw new ArgumentException("request.UserName must be set", "request");
+            }
+            List<User> users = User.SearchUsersByUserName(_userName);
+            var userCount = users.Count;
+
+            if (userCount == 0 || userCount > 1)
+            {
+                throw new ValidationException(string.Format("Could not determine exactly an User who has user name:{0}", _userName));
+            }
+            _user = users[0];
+        }
+
+    }
+}
---
+++ MarkLam/DEV10502/DeleteUserServiceCommandTest.cs
@@ -0,0 +1,96 @@
+﻿using Authentication.Contracts.Authentication;
+using Common.Contracts.Shared.Records;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payjr.Core.Identifiers;
+using Payjr.Core.ServiceCommands;
+using Payjr.Core.ServiceCommands.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payjr.Core.Test.ServiceCommands.Authentication
+{
+     [TestClass]
+    public class DeleteUserServiceCommandTest : TestBase2
+    {
+         [TestInitialize]
+         public void InitializeTest()
+         {
+             MyTestInitialize();
+         }
+         [TestMethod]
+         public void ExecuteTestSuccessful_WithRoleTeen()
+         {
+             var request = CreateLocalRequest(false);
+             var target = new DeleteUserServiceCommand(ProviderFactory);
+             var result = target.Execute(request);
+             Assert.IsTrue(result.Status.IsSuccessful);
+             Assert.IsTrue(string.IsNullOrWhiteSpace(result.Status.ErrorMessage));
+             Assert.AreEqual(Result.Success, result.Result);
+             Assert.AreEqual(new UserIdentifier(_teen.UserName).Identifier, result.Data);
+         }
+
+         [TestMethod]
+         public void ExecuteTestSuccessful_WithRoleParent()
+         {
+             var request = CreateLocalRequest(true);
+             var target = new DeleteUserServiceCommand(ProviderFactory);
+             var result = target.Execute(request);
+             Assert.IsTrue(result.Status.IsSuccessful);
+             Assert.IsTrue(string.IsNullOrWhiteSpace(result.Status.ErrorMessage));
+             Assert.AreEqual(Result.Success, result.Result);
+             Assert.AreEqual(new UserIdentifier(_parent.UserName).Identifier, result.Data);
+         }
+
+         [TestMethod]
+         public void ExecuteTestFail_UserNameEmtpy()
+         {
+             var request = CreateLocalRequest(true);
+             request.UserName = string.Empty;
+             var target = new DeleteUserServiceCommand(ProviderFactory);
+             var result = target.Execute(request);
+             Assert.IsFalse(result.Status.IsSuccessful);
+             Assert.IsTrue(result.Status.ErrorMessage.Contains("request.UserName must be set"));
+         }
+         [TestMethod]
+         public void ExecuteTestFail_UserNotExists()
+         {
+             var request = CreateLocalRequest(true);
+             request.UserName = "User Not Exists";
+             var target = new DeleteUserServiceCommand(ProviderFactory);
+             var result = target.Execute(request);
+             Assert.IsFalse(result.Status.IsSuccessful);
+             Assert.IsTrue(result.Status.ErrorMessage.Contains(string.Format("Could not determine exactly an User who has user name:{0}",request.UserName)));
+         }
+        
+        #region helper
+         private LocalRequest CreateLocalRequest(bool initParentRole)
+         {
+             var request = new LocalRequest
+             {
+                 Configuration = new RetrievalConfigurationRecord { ApplicationKey = Guid.NewGuid(),BrandingKey = Guid.NewGuid() },
+                 Header =  new RequestHeaderRecord { CallerName = "IAuthentication.DeleteUser"},
+                 
+                 UserName = (initParentRole == true)? _parent.UserName: _teen.UserName
+                 //this is old password user inputed
+             };
+             return request;
+         }
+         private LocalRequest CreateLocalRequestWithRoleIsNotParentOrTeen()
+         {
+             _user.RoleType = Entity.RoleType.Admin;
+             var request = new LocalRequest
+             {
+                 Configuration = new RetrievalConfigurationRecord { ApplicationKey = Guid.NewGuid(), BrandingKey = Guid.NewGuid() },
+                 Header = new RequestHeaderRecord { CallerName = "IAuthentication.DeleteUser" },
+
+                 UserName = _user.UserName
+             };
+             return request;
+         }
+        #endregion
+
+    }
+}
