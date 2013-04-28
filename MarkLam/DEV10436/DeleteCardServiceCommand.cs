c4d1090817b4628428220525e35d1ca7adda1b74
Mark Lam <lamma@carlabcorp.com>
2013-04-29 01:44:31 +0700

    DEV 10436 - Review 1
    Nho Adam review giup task DEV 10436

---
+++ MarkLam/DEV10436/DeleteCardServiceCommand.cs
@@ -0,0 +1,117 @@
+﻿using Common.Contracts.CreditCard.Records;
+using Common.Contracts.CreditCard.Requests;
+using Common.Contracts.CreditCard.Responses;
+using Payjr.Core.FinancialAccounts;
+using Payjr.Core.Identifiers;
+using Payjr.Core.Providers;
+using Payjr.Core.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payjr.Core.ServiceCommands.CreditCardProcessing
+{
+    public class DeleteCardServiceCommand:ProviderServiceCommandBase<DeleteCardRequest, DeleteCardResponse>
+    {
+        public DeleteCardServiceCommand(IProviderFactory providers) : base(providers) { }
+        private List<DeleteCardRecord> _deleteCardRecords;
+        protected override bool OnExecute(DeleteCardResponse response)
+        {
+            foreach (var deleteCardRecord in _deleteCardRecords)
+            {
+                // preparing record response
+                var responseRecord = new DeleteCardResponseRecord
+                {
+                    AccountIdentifier = deleteCardRecord.AccountIdentifier
+                };
+
+                User ownerUser= null;
+                CreditCardAccount creditCardAccount = this.RetrieveCreditCardAccountByID(deleteCardRecord.AccountIdentifier, out ownerUser);
+                if (creditCardAccount == null)
+                {
+                    continue;
+                }
+                var deletedSuccessful = creditCardAccount.DeleteAccount();
+                // set flag is deleted/undeleted for response
+                if (deletedSuccessful)
+                {
+                    deletedSuccessful = creditCardAccount.Save(null);
+                    if (deletedSuccessful)
+                        deletedSuccessful = ownerUser.Save(null);
+                }
+                responseRecord.IsDeleted = deletedSuccessful;
+                // add processed record to response
+                response.Respones.Add(responseRecord);
+            }
+            return true;
+        }
+
+        protected override void Validate(DeleteCardRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request", "request must be set value");
+            }
+
+            var deleteCardRecords = request.Requests;
+            if (deleteCardRecords == null || deleteCardRecords.Count == 0)
+            {
+                throw new ArgumentException("DeleteCardsRecords must be set", "request.AddCardsRecords");
+            }
+            _deleteCardRecords = new List<DeleteCardRecord>();
+            var pos = -1;
+            var count = 0;
+            foreach (var deleteCard in deleteCardRecords)
+            {
+                pos++;
+                if (string.IsNullOrWhiteSpace(deleteCard.AccountIdentifier))
+                {
+                    Log.Debug("skiped request.DeleteCardRecords[{0}] because AccountIdentifier is null or empty", pos);
+                    continue;
+                }
+                _deleteCardRecords.Add(deleteCard);
+                count++;
+            }
+            //if (count < deleteCardRecords.Count)
+            Log.Debug("Ending validate the request. {0}/{1} record(s) passed validation", count, deleteCardRecords.Count);
+        }
+
+        #region helper
+        private CreditCardAccount RetrieveCreditCardAccountByID(string cardIdentifier, out User userOwner)
+        {
+            CreditCardAccount creditCardAcount = null;
+            userOwner = null;
+            try
+            {
+                Guid creditCardID = new CreditCardIdentifier(cardIdentifier).PersistableID;
+                creditCardAcount = CreditCardAccount.RetrieveCreditCardAccountByID(creditCardID);
+                if (creditCardAcount == null)
+                {
+                    Log.Debug("Could not found Credit Card with ID={0}", creditCardID);
+                    return null;
+                }
+                if (!creditCardAcount.UserID.HasValue)
+                {
+                    Log.Debug("Could not determine the user who associated with the credit card - ID={0}", creditCardID);
+                    return null;
+                }
+                // get teen info
+                userOwner = User.RetrieveUser(creditCardAcount.UserID.Value);
+                if (userOwner == null)
+                {
+                    Log.Debug("Could not retrieve the User - ID={0}", creditCardAcount.UserID.Value);
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Debug(ex.Message);
+                return null;
+            }
+            return creditCardAcount;
+        }
+        #endregion
+    }
+}
---
+++ MarkLam/DEV10436/DeleteCardServiceCommandTest.cs
@@ -0,0 +1,167 @@
+﻿using Common.Contracts.CreditCard.Records;
+using Common.Contracts.CreditCard.Requests;
+using Common.Contracts.Shared.Records;
+using Common.Types;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payjr.Core.FinancialAccounts;
+using Payjr.Core.Identifiers;
+using Payjr.Core.Modules;
+using Payjr.Core.ServiceCommands.CreditCardProcessing;
+using Payjr.Core.Users;
+using Payjr.Util.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payjr.Core.Test.ServiceCommands.CreditCardProcessing
+{
+    [TestClass]
+    public class DeleteCardServiceCommandTest : TestBase2
+    {
+       // private PrepaidModule _prepaidModule;
+        private string _creditCardIdentifier1;
+        private string _creditCardIdentifier2;
+        [TestInitialize]
+        public override void MyTestInitialize()
+        {
+            base.MyTestInitialize();
+
+            TestEntityFactory.CreateCreditCardAccount(_parent, true, Entity.AccountStatus.AllowMoneyMovement, MockCreator.CreateFakeCreditCardNumber());
+            var ppAcctId1 = _parent.FinancialAccounts.CreditCardAccounts[0].AccountID;
+            _creditCardIdentifier1 = new CreditCardIdentifier(ppAcctId1).DisplayableIdentifier;
+            _creditCardIdentifier2 = new CreditCardIdentifier(ppAcctId1).DisplayableIdentifier;
+        }
+
+        [TestMethod]
+        public void Execute_Successful()
+        {
+            var request = CreateDeleteCardRequest(true);
+            //check credit cart when it's just created.
+            foreach (var card in request.Requests)
+            {
+                var isCardDeleted = IsDeletedCreditCard(card.AccountIdentifier);
+                Assert.IsNotNull(isCardDeleted);
+                Assert.IsFalse(isCardDeleted.Value);
+            }
+            var target = new DeleteCardServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.IsNotNull(result.Status);
+            Assert.IsTrue(result.Status.IsSuccessful);
+            Assert.AreEqual(2,result.Respones.Count);
+            //re-check after excute delete card process
+            foreach (var card in result.Respones)
+            {
+                var isCardDeleted = IsDeletedCreditCard(card.AccountIdentifier);
+                Assert.IsNotNull(isCardDeleted);
+                Assert.IsTrue(isCardDeleted.Value);
+            }
+        }
+        [TestMethod]
+        public void Execute_Successful_With1CardReturned()
+        {
+            var request = CreateDeleteCardRequest(true);
+            //check credit cart when it's just created.
+            Assert.AreEqual(2, request.Requests.Count);
+            foreach (var card in request.Requests)
+            {
+                var isCardDeleted = IsDeletedCreditCard(card.AccountIdentifier);
+                Assert.IsNotNull(isCardDeleted);
+                Assert.IsFalse(isCardDeleted.Value);
+            }
+            //update to the second card is not found.
+            request.Requests[1].AccountIdentifier = "CardIsNotFound";
+            var target = new DeleteCardServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.IsNotNull(result.Status);
+            Assert.IsTrue(result.Status.IsSuccessful);
+            Assert.AreEqual(1,result.Respones.Count);
+            //re-check after excute delete card process
+            foreach (var card in result.Respones)
+            {
+                var isCardDeleted = IsDeletedCreditCard(card.AccountIdentifier);
+                Assert.IsNotNull(isCardDeleted);
+                Assert.IsTrue(isCardDeleted.Value);
+            }
+        }
+       [TestMethod]
+        public void Execute_Failure_RequestIsNull()
+        {
+            DeleteCardRequest request = null;
+            var target = new DeleteCardServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.AreEqual(0, result.Respones.Count);
+            Assert.IsNotNull(result.Status);
+            Assert.IsFalse(result.Status.IsSuccessful);
+            Assert.IsTrue(result.Status.ErrorMessage.StartsWith("request must be set value"));
+        }
+        
+        [TestMethod]
+        public void Execute_Failure_RequestRecordIsEmpty()
+        {
+            var request = CreateDeleteCardRequest(false);
+            Assert.AreEqual(0, request.Requests.Count);
+            var target = new DeleteCardServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.AreEqual(0, result.Respones.Count);
+            Assert.IsNotNull(result.Status);
+            Assert.IsFalse(result.Status.IsSuccessful);
+            Assert.IsTrue(result.Status.ErrorMessage.StartsWith("DeleteCardsRecords must be set"));
+        }
+        [TestMethod]
+        public void Execute_Failure_AccIdentifierNotFound()
+        {
+            var request = CreateDeleteCardRequest(true);
+            Assert.AreEqual(2, request.Requests.Count);
+            //update to the both card2 is not found.
+            request.Requests[0].AccountIdentifier = "CardIsNotFound1";
+            request.Requests[1].AccountIdentifier = "CardIsNotFound2";
+            var target = new DeleteCardServiceCommand(ProviderFactory);
+            var result = target.Execute(request);
+            Assert.AreEqual(0, result.Respones.Count);
+            Assert.IsNotNull(result.Status);
+            Assert.IsTrue(result.Status.IsSuccessful);
+        }
+        #region helper methods
+
+        private DeleteCardRequest CreateDeleteCardRequest(bool initDeleteCardRecord)
+        {
+            var result = new DeleteCardRequest
+            {
+                Header = new RequestHeaderRecord
+                {
+                    CallerName = "DeleteCardServiceCommandTest"
+                }
+            };
+            if (initDeleteCardRecord)
+            {
+                //record 1
+                result.Requests.Add(
+                new DeleteCardRecord
+                {
+                    AccountIdentifier = _creditCardIdentifier1,
+                });
+                //record2
+                result.Requests.Add(
+                new DeleteCardRecord
+                {
+                    AccountIdentifier = _creditCardIdentifier2,
+                });
+            }
+            return result;
+        }
+        private bool? IsDeletedCreditCard(string cardIdentifier)
+        {
+            Guid creditCardID = new CreditCardIdentifier(cardIdentifier).PersistableID;
+            CreditCardAccount creditCardAcount = CreditCardAccount.RetrieveCreditCardAccountByID(creditCardID);
+            if (creditCardAcount != null && creditCardAcount.MarkedForDeletion == true && creditCardAcount.Status == Entity.AccountStatus.DoNotAllowMoneyMovement)
+                return true;
+            else if (creditCardAcount != null && creditCardAcount.MarkedForDeletion == false && creditCardAcount.Status == Entity.AccountStatus.AllowMoneyMovement)
+                return false;
+            return null;
+        }
+        #endregion
+
+    }
+}
